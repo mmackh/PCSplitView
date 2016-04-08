0e7ad4c2895188a6b32c9b228d9e584cc7ca213f
Maximilian Mackh <mmackh@gmail.com>
2016-04-08 12:18:12 +0200

    Update PCSplitView.m

--- PCSplitView.m
+++ PCSplitView.m
@@ -1,10 +1,91 @@
+//
+//  PCSplitView.m
+//  Ordervisto
+//
+//  Created by Maximilian Mackh on 30/08/15.
+//  Copyright © 2015 Professional Consulting & Trading GmbH. All rights reserved.
+//
+
 #import "PCSplitView.h"
+
+NSInteger const PCSplitViewExcludeLayoutViewTag = 260890;
+NSInteger const PCSplitViewDisableClipOnViewTag = 110292;
+NSInteger const PCSplitViewSendToBackAndDisableClipOnViewTag = 160894;
+
+@interface PCSplitView ()
+
+@property (nonatomic) NSArray *originalSubviews;
+
+@end
 
 @implementation PCSplitView
 
+- (void)setSubviewLayout:(NSString *)subviewLayout direction:(PCSplitViewDirection)splitDirection
+{
+    self.splitViewDirection = splitDirection;
+    
+    NSMutableArray *subviewRatiosMutable = [NSMutableArray new];
+    NSMutableArray *subviewFixedValuesMutable = [NSMutableArray new];
+    for (NSString *layout in [subviewLayout componentsSeparatedByString:@","])
+    {
+        NSArray *layoutSplit = [layout componentsSeparatedByString:@"*"];
+        [subviewRatiosMutable addObject:[layoutSplit firstObject]];
+        [subviewFixedValuesMutable addObject:(![layout containsString:@"*"])?@"-1":[layoutSplit lastObject]];
+    }
+    
+    self.subviewFixedValues = subviewFixedValuesMutable.copy;
+    self.subviewRatios = subviewRatiosMutable.copy;
+}
+
+- (NSString *)subviewLayout
+{
+    NSMutableArray *subviewLayoutMutable = [NSMutableArray new];
+    NSInteger idx = 0;
+    
+    BOOL usesFixedValues = self.subviewFixedValues.count;
+    
+    for (id value in self.subviewRatios)
+    {
+        id fixedValue = (!usesFixedValues) ? @"-1" : self.subviewFixedValues[idx];
+        [subviewLayoutMutable addObject:[NSString stringWithFormat:@"%@*%@",value,fixedValue]];
+        idx += 1;
+    }
+    return [subviewLayoutMutable componentsJoinedByString:@","].copy;
+}
+
+- (void)layoutIfNeeded
+{
+    if (self.preventAnimations)
+    {
+        [CATransaction begin];
+        [CATransaction setDisableActions:YES];
+        [super layoutIfNeeded];
+        [CATransaction commit];
+    }
+    else
+    {
+        [super layoutIfNeeded];
+    }
+}
+
 - (void)layoutSubviews
 {
+    if (self.preventAnimations)
+    {
+        [CATransaction begin];
+        [CATransaction setDisableActions:YES];
+    }
+    
     [super layoutSubviews];
+    
+    if (!self.originalSubviews || self.originalSubviews.count != self.subviews.count) self.originalSubviews = self.subviews;
+    
+    NSMutableArray *subviewsMutable = [NSMutableArray new];
+    for (UIView *subview in self.originalSubviews)
+    {
+        if (subview.tag == PCSplitViewExcludeLayoutViewTag) continue;
+        [subviewsMutable addObject:subview];
+    }
     
     if (!self.subviewRatios.count) return;
     
@@ -12,6 +93,10 @@
     
     CGFloat topLayoutGuideLength = (self.parentViewController) ? self.parentViewController.topLayoutGuide.length : 0.0;
     CGFloat bottomLayoutGuideLength = (self.parentViewController) ? self.parentViewController.bottomLayoutGuide.length : 0.0;
+    
+    NSArray *fixedValues = self.subviewFixedValues;
+    NSArray *ratios = self.subviewRatios;
+    CGFloat padding = self.subviewPadding;
     
     NSInteger counter = 0;
     CGFloat offsetTracker = (hz)?0.0:topLayoutGuideLength;
@@ -21,10 +106,10 @@
     CGFloat ratioLossValue = 0.0;
     
     CGFloat fixedValuesSum = 0.0;
-    for (NSNumber *fixedValue in self.subviewFixedValues)
+    for (NSNumber *fixedValue in fixedValues)
     {
         CGFloat fixedValueFloat = [fixedValue floatValue];
-        CGFloat ratio = [self.subviewRatios[ratioLossIndex] floatValue];
+        CGFloat ratio = [ratios[ratioLossIndex] floatValue];
         ratioLossIndex++;
         
         ratioTargetCount++;
@@ -35,17 +120,19 @@
         ratioLossValue += ratio;
     }
     
+    NSInteger subviewPadding = (int)padding;
+    
     CGFloat width = self.bounds.size.width - ((hz)?fixedValuesSum : 0.0);
     CGFloat height = self.bounds.size.height - (topLayoutGuideLength + bottomLayoutGuideLength) - ((hz)?0.0 : fixedValuesSum);
     
-    for (UIView *childView in self.subviews)
+    for (UIView *childView in subviewsMutable)
     {
-        CGFloat ratio = [self.subviewRatios[counter] floatValue] + ((ratioLossValue>0)?(ratioLossValue / ratioTargetCount):0.0);
+        CGFloat ratio = [ratios[counter] floatValue] + ((ratioLossValue>0)?(ratioLossValue / ratioTargetCount):0.0);
         
         CGFloat fixedValue = -1;
-        if (self.subviewFixedValues.count)
+        if (fixedValues.count)
         {
-            fixedValue = [self.subviewFixedValues[counter] floatValue];
+            fixedValue = [fixedValues[counter] floatValue];
         }
         
         CGRect childFrame = CGRectMake((hz)?offsetTracker:0, (hz)?topLayoutGuideLength:offsetTracker, (hz)?(width * ratio) : width, (hz)?height : (height * ratio));
@@ -63,12 +150,24 @@
         
         offsetTracker += (hz)?childFrame.size.width : childFrame.size.height;
         
-        childView.frame = childFrame;
-        childView.clipsToBounds = YES;
-        [self clipViewToEdge:childView removeConstraints:NO];
+        childView.frame = CGRectInset(childFrame, subviewPadding, subviewPadding);;
+        childView.clipsToBounds = (childView.tag != PCSplitViewDisableClipOnViewTag);
+    
+        if (childView.tag == PCSplitViewSendToBackAndDisableClipOnViewTag)
+        {
+            [self sendSubviewToBack:childView];
+            childView.clipsToBounds = NO;
+        }
         
         counter++;
     }
+    
+    if (self.preventAnimations) [CATransaction commit];
+}
+
+- (void)addSubview:(UIView *)view
+{
+    [super addSubview:view];
 }
 
 - (void)clipViewToEdge:(UIView *)view removeConstraints:(BOOL)removeConstraints
@@ -80,28 +179,84 @@
             [view removeConstraint:constraint];
         }
     }
-    
-    view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    view.translatesAutoresizingMaskIntoConstraints = YES;
+}
+
+- (void)snapToSuperview
+{
+    [self snapToSuperviewRegardingLayoutGuides:NO parentViewController:nil];
 }
 
 - (void)snapToSuperviewRegardingLayoutGuides:(BOOL)regardLayoutGuides parentViewController:(UIViewController *)parentViewController;
 {
     self.parentViewController = parentViewController;
     
+    [self snapToSuperviewRegardingLayoutGuides:regardLayoutGuides paddingLeft:0 paddingRight:0 paddingBottom:0 paddingTop:0];
+}
+
+- (void)snapToSuperviewRegardingLayoutGuides:(BOOL)regardLayoutGuides paddingLeft:(CGFloat)paddingLeft paddingRight:(CGFloat)paddingRight paddingBottom:(CGFloat)paddingBottom paddingTop:(CGFloat)paddingTop
+{
     self.translatesAutoresizingMaskIntoConstraints = NO;
     
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:self.superview attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeading multiplier:1.0f constant:0.0f]];
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:self.superview attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTrailing multiplier:1.0f constant:0.0f]];
+    UIView *superview = self.superview;
     
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:(regardLayoutGuides)?(id)self.parentViewController.topLayoutGuide : self.superview attribute:(regardLayoutGuides)?NSLayoutAttributeBottom:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0f constant:0.0f]];
-    [self.superview addConstraint:[NSLayoutConstraint constraintWithItem:(regardLayoutGuides)?(id)self.parentViewController.bottomLayoutGuide : self.superview attribute:(regardLayoutGuides)?NSLayoutAttributeTop:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0f constant:0.0f]];
+    [superview addConstraints:@[
+                                
+        [NSLayoutConstraint constraintWithItem:self
+                                     attribute:NSLayoutAttributeTop
+                                     relatedBy:NSLayoutRelationEqual
+                                        toItem:(regardLayoutGuides) ? (id)self.parentViewController.topLayoutGuide : superview
+                                     attribute:(regardLayoutGuides) ?NSLayoutAttributeBottom : NSLayoutAttributeTop
+                                    multiplier:1.0
+                                      constant:paddingTop],
+        
+        [NSLayoutConstraint constraintWithItem:self
+                                     attribute:NSLayoutAttributeLeft
+                                     relatedBy:NSLayoutRelationEqual
+                                        toItem:superview
+                                     attribute:NSLayoutAttributeLeft
+                                    multiplier:1.0
+                                      constant:paddingLeft],
+        
+        [NSLayoutConstraint constraintWithItem:self
+                                     attribute:NSLayoutAttributeBottom
+                                     relatedBy:NSLayoutRelationEqual
+                                        toItem:(regardLayoutGuides) ? (id)self.parentViewController.bottomLayoutGuide : superview
+                                     attribute:(regardLayoutGuides)?NSLayoutAttributeTop : NSLayoutAttributeBottom
+                                    multiplier:1.0
+                                      constant:-paddingBottom],
+        
+        [NSLayoutConstraint constraintWithItem:self
+                                     attribute:NSLayoutAttributeRight
+                                     relatedBy:NSLayoutRelationEqual
+                                        toItem:superview
+                                     attribute:NSLayoutAttributeRight
+                                    multiplier:1
+                                      constant:-paddingRight],
+        
+    ]];
+    
 }
 
 - (void)invalidateLayout
 {
+    self.originalSubviews = nil;
+    
     [self setNeedsLayout];
     [self layoutIfNeeded];
+}
+
+- (void)addEmptySubview
+{
+    UIView *subview = [[UIView alloc] initWithFrame:CGRectZero];
+    subview.tag = PCSplitViewExcludeLayoutViewTag;
+    [self addSubview:subview];
+}
+
+- (void)addSubviewWithColor:(UIColor *)color
+{
+    UIView *view = [[UIView alloc] init];
+    if (color) view.backgroundColor = color;
+    [self addSubview:view];
 }
 
 @end
